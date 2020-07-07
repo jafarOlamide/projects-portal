aac58c483ce01b75ac1f85a63fb1990d067222a2
jafarOlamide <31123468+jafarOlamide@users.noreply.github.com>
2020-07-08 00:16:42 +0100

    Add files via upload

---
+++ includes/headLinks.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/style_ongoing_projects.css">
+<link rel="stylesheet" type="text/css" href="css/style_all_projects.css">
+<link rel="stylesheet" type="text/css" href="css/style_dashboard.css">
+<!-- Custom CSS -->
+<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+<script src="js/ongoing_projects_script.js"></script>
+<script src="js/navbar.js"></script>
