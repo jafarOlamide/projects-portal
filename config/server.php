675f8c0abaf5caa3e66b7326a2b9ebe8dad39a4e
jafarolamide <jafarolamidekale@gmail.com>
2020-07-21 02:13:52 +0100

    bug fix

--- config/server.php
+++ config/server.php
@@ -1,6 +1,5 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', '', 'officeprojectscheduler');
-//$connection = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b0ecebaa70d2bd', 'f1ed49ea', 'heroku_c64291e4f1c514e');
 
         if (!$connection) {
             die("Database Connection Failed: " . mysqli_error());
